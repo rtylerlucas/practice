bb901f6d0b09fb681e7505ca4eece2ca9ed77423
rtylerlucas <rtylerlucas@gmail.com>
2016-07-26 08:23:38 -0700

    Working through more problems

---
+++ EPI/src/chapter/eleven/ArrayEntry.java
@@ -0,0 +1,12 @@
+package chapter.eleven;
+
+public class ArrayEntry {
+	public int value;
+	public int arrayId;
+	
+	public ArrayEntry(int value, int arrayId){
+		this.value = value;
+		this.arrayId = arrayId;
+	}
+
+}
